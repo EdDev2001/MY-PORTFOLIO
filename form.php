0388da8ff1215633b7e8c7b15537870eca040410
EdDev <133908849+EdDev2001@users.noreply.github.com>
2023-10-31 07:24:43 +0800

    Add files via upload

---
+++ form.php
@@ -0,0 +1,27 @@
+<?php 
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$phone = $_POST['phone'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'index.html';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Phone: $phone.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'ednelpadernal4@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+?>
